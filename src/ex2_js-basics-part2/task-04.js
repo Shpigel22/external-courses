22c2c83546919cdbd200cfef46c9cef9f9f5b1ef
CowboyTanaka <tanakabusy@mail.ru>
2025-07-24 13:46:53 +0300

    feat:task-04

---
+++ src/ex2_js-basics-part2/task-04.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-plusplus */
+function validater(arr) {
+  const a = arr[0];
+  if (arr.length === 0) return true;
+  for (let i = 1; i < arr.length; i++) {
+    const number = arr[i];
+    if (number !== a || isNaN(number)) {
+      return false;
+    }
+  }
+  return true;
+}
+module.exports = validater;
+console.log(validater([1, 2, 3, 4, 5, 1, 3, 4, 6]));
+console.log(validater([5, 5, 5, 5]));
+console.log(validater([1, 1, 1, NaN, 1]));
+console.log(validater([0, 0, 0, 0]));
+console.log(validater([NaN, NaN, NaN]));
+console.log(validater([]));
+console.log(validater([null, null, null, undefined]));
