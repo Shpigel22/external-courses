c85556ce4405d07b99ad2f8f0755565b6695999b
CowboyTanaka <tanakabusy@mail.ru>
2025-09-04 19:15:20 +0300

    feat: task-02

---
+++ src/ex6_js-array-methods/task-02.js
@@ -0,0 +1,16 @@
+function exists(array, callback) {
+  if (!Array.isArray(array)) {
+    return false;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    const item = array[i];
+    if (callback(item, i, array)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+module.exports = exists;
